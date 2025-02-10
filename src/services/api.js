9fa8836b396eb17d4be795e88ccc08c14f57791f
Ying Song <sylviayrsong@gmail.com>
2025-02-09 22:10:17 -0500

    React-based practice web application. It uses JSX, Node.js, and TMDb API to fetch movie data.

---
+++ src/services/api.js
@@ -0,0 +1,21 @@
+// https://developer.themoviedb.org/reference/movie-popular-list
+// https://developer.themoviedb.org/reference/search-movie
+
+const API_KEY = "e14ee2daf63e54a36b100fb277033856";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
